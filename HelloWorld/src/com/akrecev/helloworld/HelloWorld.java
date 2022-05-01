fea5d7b5b9f89f1ebecd5818758bde102b2fd2dd
akrecev <98045955+akrecev@users.noreply.github.com>
2022-05-02 00:04:31 +0300

    2

--- HelloWorld/src/com/akrecev/helloworld/HelloWorld.java
+++ HelloWorld/src/com/akrecev/helloworld/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		String message = "Hello world!";
 		System.out.println(message);
+		String message2 = "Hello world! Hello world!";
+		System.out.println(message2);
 
 	}
 
