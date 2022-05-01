809657da5eaae470bf7e8a73f4b760d7a1fb3fb1
akrecev <akrecev@gmail.com>
2022-05-02 00:39:32 +0300

    3

--- HelloWorld/src/com/akrecev/helloworld/HelloWorld.java
+++ HelloWorld/src/com/akrecev/helloworld/HelloWorld.java
@@ -5,8 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		String message = "Hello world!";
 		System.out.println(message);
-		String message2 = "Hello world! Hello world!";
-		System.out.println(message2);
 
 	}
 
