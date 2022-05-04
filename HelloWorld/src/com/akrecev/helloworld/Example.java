8f9e4592fde598b74af8e0cdf3bf7c652322576d
akrecev <akrecev@gmail.com>
2022-05-04 15:32:41 +0300

    6

---
+++ HelloWorld/src/com/akrecev/helloworld/Example.java
@@ -0,0 +1,19 @@
+package com.akrecev.helloworld;
+
+/*
+ Это пример простой программы на Java.
+ Присвойте файлу с исходным кодом имя Example.java.
+ */
+
+public class Example {
+	
+//	выполнение программы в Java сачинается с вызова метода main()
+
+	public static void main(String[] args) {
+		
+//		вывод в консоль надписи "Java rules the intrnet!"
+		System.out.println("Java rules the intrnet!");
+
+	}
+
+}
