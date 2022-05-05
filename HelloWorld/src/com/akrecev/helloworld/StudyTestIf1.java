862545dcb296ab4d06a2c8cd0f3a2b9eed8468d2
akrecev <akrecev@gmail.com>
2022-05-05 18:09:23 +0300

    05.05.2022 - 1

---
+++ HelloWorld/src/com/akrecev/helloworld/StudyTestIf1.java
@@ -0,0 +1,43 @@
+package com.akrecev.helloworld;
+
+import java.util.Scanner;
+
+public class StudyTestIf1 {
+
+	public static void main(String[] args) {
+
+		Scanner myscan = new Scanner(System.in);
+
+		System.out.println("Привет!");
+		System.out.println("Сколько будет 25*5 ?");
+
+		int x = 0;
+		x = myscan.nextInt(); // считать int с клавиатуры и записать в x
+
+		if (x == 125) {
+			System.out.println("Да, это круто!");
+		} else {
+			System.out.println("Абсолютная ложь!");
+		}
+
+		System.out.println("Назови столицу Парагвая");
+		String capitalOfParaguay = myscan.next();
+		if (capitalOfParaguay.equals("Асунсьон")) {
+			System.out.println("Верно!");
+		} else {
+			System.out.println("Нет, это  Асунсьон!");
+		}
+
+		System.out.println("Какая самая яркая звезда на небе ночью?");
+		String brightStar = myscan.next().toLowerCase();
+		if (brightStar.equals("сириус")) {
+			System.out.println("Точно!");
+		} else {
+			System.out.println("Это точно не " + brightStar + "!");
+		}
+
+		myscan.close();
+
+	}
+
+}
